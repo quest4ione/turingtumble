66bfc7190f86528da4d182af058a2e74d20f45be
quest4ione <196376222+quest4ione@users.noreply.github.com>
2025-04-21 18:10:36 +0200

    Added gearbit simulation

---
+++ src/simulator/gearbit.rs
@@ -0,0 +1,82 @@
+use std::{
+    cell::{Cell, RefCell},
+    fmt::Debug,
+    rc::Rc,
+};
+
+use super::Orientation;
+
+pub struct GearBit {
+    shared_orientation: Rc<Cell<Orientation>>,
+    pub left: Option<Rc<RefCell<GearBit>>>,
+    pub right: Option<Rc<RefCell<GearBit>>>,
+}
+
+impl Debug for GearBit {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        fn inner(
+            gearbit: &GearBit,
+            f: &mut std::fmt::Formatter,
+            seen: &mut Vec<*mut Orientation>,
+        ) -> std::fmt::Result {
+            seen.push(gearbit.shared_orientation.as_ptr());
+
+            write!(
+                f,
+                "GearBit {{ orientation: [{}]={:?}",
+                seen.iter()
+                    .position(|x| x == &gearbit.shared_orientation.as_ptr())
+                    .unwrap(),
+                gearbit.shared_orientation.get(),
+            )?;
+
+            if let Some(left) = &gearbit.left {
+                write!(f, ", left: ")?;
+                inner(&left.borrow(), f, seen)?;
+            }
+
+            if let Some(right) = &gearbit.right {
+                write!(f, ", right: ")?;
+                inner(&right.borrow(), f, seen)?;
+            }
+
+            write!(f, " }}")
+        }
+
+        inner(self, f, &mut Vec::new())
+    }
+}
+
+impl GearBit {
+    pub fn new(orientation: Orientation) -> Rc<RefCell<Self>> {
+        Rc::new(RefCell::new(Self {
+            left: None,
+            right: None,
+            shared_orientation: Rc::new(Cell::new(orientation)),
+        }))
+    }
+
+    pub fn new_shared(&self) -> Rc<RefCell<Self>> {
+        Rc::new(RefCell::new(Self {
+            left: None,
+            right: None,
+            shared_orientation: self.shared_orientation.clone(),
+        }))
+    }
+
+    pub fn trigger(&mut self) -> Option<Rc<RefCell<GearBit>>> {
+        let new_orientation = match self.shared_orientation.get() {
+            Orientation::Left => Orientation::Right,
+            Orientation::Right => Orientation::Left,
+        };
+        self.shared_orientation.set(new_orientation);
+        match new_orientation {
+            Orientation::Left => self.left.clone(),
+            Orientation::Right => self.right.clone(),
+        }
+    }
+
+    pub fn orientation(&self) -> Orientation {
+        self.shared_orientation.get()
+    }
+}
---
+++ src/simulator/mod.rs
@@ -0,0 +1,60 @@
+mod gearbit;
+pub use gearbit::GearBit;
+use std::{cell::RefCell, fmt::Debug, rc::Rc};
+
+#[derive(PartialEq, Eq, Clone, Copy, Debug)]
+pub enum Orientation {
+    Left,
+    Right,
+}
+
+pub enum State {
+    Simulating,
+    Intercepted,
+}
+
+#[derive(Default)]
+pub struct Simulator {
+    left_root: Option<Rc<RefCell<GearBit>>>,
+    right_root: Option<Rc<RefCell<GearBit>>>,
+    curr: Option<Rc<RefCell<GearBit>>>,
+}
+
+impl Simulator {
+    pub fn new() -> Self {
+        Default::default()
+    }
+
+    pub fn with_roots(
+        left_root: Option<Rc<RefCell<GearBit>>>,
+        right_root: Option<Rc<RefCell<GearBit>>>,
+    ) -> Self {
+        Self {
+            left_root,
+            right_root,
+            curr: None,
+        }
+    }
+
+    pub fn state(&self) -> State {
+        match self.curr {
+            Some(_) => State::Simulating,
+            None => State::Intercepted,
+        }
+    }
+
+    pub fn trigger(&mut self, side: Orientation) {
+        self.curr = match side {
+            Orientation::Left => self.left_root.clone(),
+            Orientation::Right => self.right_root.clone(),
+        }
+    }
+
+    pub fn step(&mut self) -> State {
+        self.curr = match &self.curr {
+            None => return State::Intercepted,
+            Some(curr) => curr.borrow_mut().trigger().clone(),
+        };
+        self.state()
+    }
+}
